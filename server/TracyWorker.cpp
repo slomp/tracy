500d4961a3fa47dc510ee15ae5d66456599eed3e
Marcos Slomp <mslomp@gmail.com>
2026-03-13 14:57:56 -0700

    isolatincg stochastic render experiment

--- server/TracyWorker.cpp
+++ server/TracyWorker.cpp
@@ -2886,10 +2886,6 @@ void Worker::Exec()
     }
 
     t0 = std::chrono::high_resolution_clock::now();
-    static uint32_t renderStarveScore = 0;
-    static float pendingReadScore = 0.0f;
-    static float pendingReadWeight = 4.0f;
-    static uint32_t handoffRng = 1;
 
     for(;;)
     {
@@ -2915,22 +2911,27 @@ void Worker::Exec()
         const char* end = ptr + netbuf.size;
 
         {
+            bool didHandoff = false;
             const auto tHandoffStart = std::chrono::high_resolution_clock::now();
             std::unique_lock<std::mutex> lk( m_data.lock );
-            bool didHandoff = false;
+#if 0 /* STOCHASTIC RENDER HAND-OFF */
             if( m_data.mainThreadWantsLock )
             {
+                static float pendingReadScore = 0.0f;
+                static float pendingReadWeight = 4.0f;
                 const int pendingBytes = netbuf.size + GetSocketRecvQueueBytes();
                 const int recvBufSize = m_sock.GetRecvBufSize();
                 const float pending = (float)pendingBytes / (float)recvBufSize;
                 pendingReadScore += pending * pendingReadWeight;
 
+                static uint32_t renderStarveScore = 0;
+                static uint32_t handoffRng = 1;
                 handoffRng = handoffRng * 1103515245u + 12345u;
                 const float r = ( handoffRng >> 16 ) / 65536.0f;
                 const float handoffProb = ( 1.0f + renderStarveScore ) / ( 1.0f + renderStarveScore + pendingReadScore );
                 const bool doHandoff = ( r < handoffProb );
 
-                //fprintf(hFile, "doHandoff: %d, renderStarveScore: %d, pendingReadScore: %f, pendingReadWeight: %f, r: %f, handoffProb: %f\n", doHandoff, renderStarveScore, pendingReadScore, pendingReadWeight, r, handoffProb);
+                fprintf(hFile, "doHandoff: %d, renderStarveScore: %d, pendingReadScore: %f, pendingReadWeight: %f, r: %f, handoffProb: %f\n", doHandoff, renderStarveScore, pendingReadScore, pendingReadWeight, r, handoffProb);
 
                 if( doHandoff )
                 {
@@ -2946,6 +2947,16 @@ void Worker::Exec()
                     pendingReadWeight *= 0.95f;
                 }
             }
+#else
+            if( m_data.mainThreadWantsLock )
+            {
+                // Hand over the lock to the main thread to avoid starving it.
+                // Wait for a millisecond maximum to avoid the opposite
+                // problem where main thread would never let us execute
+                m_data.lockCv.wait_for( lk, std::chrono::milliseconds( 1 ) );
+                didHandoff = true;
+            }
+#endif
             if( didHandoff )
             {
                 uint64_t handoffNs = uint64_t( std::chrono::duration_cast<std::chrono::nanoseconds>( std::chrono::high_resolution_clock::now() - tHandoffStart ).count() );
